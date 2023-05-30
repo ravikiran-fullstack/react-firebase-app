0b14ccba9019a085c0c3566afd49cd39a0226179
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-05-30 17:59:13 -0400

    added crud operation to the app, need to work on CSS and modularizing the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,134 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/Auth';
+import { auth, db } from './config/firebase';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from 'firebase/firestore';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [oscar, setOscar] = useState(true);
+
+  const [updatedOscar, setUpdatedOscar] = useState(false);
+
+  const moviesCollectionRef = collection(db, 'movies');
+
+  const getMovieList = async () => {
+    // READ THE DATA
+    // SET THE MOVIE LIST
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      console.log(filteredData);
+      setMovieList(filteredData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovieList();
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title,
+        releaseDate: year,
+        oscar,
+        userId: auth?.currentUser?.uid,
+      });
+      getMovieList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteMovie = async (id) => {
+    const movieDoc = doc(db, 'movies', id);
+    try {
+      await deleteDoc(movieDoc);
+      getMovieList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateMovieOscar = async (id) => {
+    const movieDoc = doc(db, 'movies', id);
+    try {
+      await updateDoc(movieDoc, { oscar: updatedOscar });
+      getMovieList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <h2>React with Firebase</h2>
       <Auth />
+
+      <div>
+        <input
+          type="text"
+          name="title"
+          placeholder="Movie Name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          name="title"
+          placeholder="Release Year"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        />
+        <input
+          type="checkbox"
+          name="title"
+          checked={oscar}
+          id="oscar"
+          onChange={(e) => setOscar(e.target.checked)}
+        />
+        <label htmlFor="oscar">Received an Oscar</label>
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+
+      <div>
+        {movieList &&
+          movieList.map((movie) => (
+            <div>
+              <h1 style={{ color: movie.oscar ? 'green' : 'red' }}>
+                {movie.title}
+              </h1>
+              <p>Date: {movie.releaseDate}</p>
+              <input
+                type="checkbox"
+                name="updatedOscar"
+                value={updatedOscar}
+                onChange={(e) => setUpdatedOscar(e.target.checked)}
+              />
+              <button onClick={() => updateMovieOscar(movie.id)}>Update</button>
+              <button onClick={() => deleteMovie(movie.id)}>
+                Delete Movie
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCtz5Cj7H77Wv7BmttngyfcuwrgGSEAyRQ',
@@ -15,3 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
